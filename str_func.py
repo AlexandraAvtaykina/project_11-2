5acbcdabb9aade170d3da87ca2bc7339a61baeca
Alexandra Avtaykina <justalexandra@mail.ru>
2023-07-15 19:30:39 +0300

    str_func.py file changes

--- str_func.py
+++ str_func.py
@@ -1,2 +1,6 @@
 def string_conversion(string):
     return string.upper()
+
+'''Функция делает заглавными первые буквы каждого слова в строке'''
+def string_conversion2(string):
+    return string.title()
